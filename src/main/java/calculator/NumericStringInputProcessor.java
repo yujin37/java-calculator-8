4f7be3c10a2251c67328e68679f3c6afc4ad8d13
yujin37 <yj061628@gmail.com>
2025-10-17 00:24:48 +0900

    feat: 덧셈할 문자열 사용자 입력 및 출력 처리

---
+++ src/main/java/calculator/NumericStringInputProcessor.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class NumericStringInputProcessor {
+    public static String readInputString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+    public static void printResult(String outputValue) {
+        System.out.println(outputValue);
+    }
+    public static void processNumericString() {
+        String inputString = readInputString();
+        printResult(inputString);
+    }
+}
