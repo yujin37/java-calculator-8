2ad1ef80df94db238cdd807004fe7414a48800a5
yujin37 <yj061628@gmail.com>
2025-10-17 02:19:04 +0900

    fix: 기본 구분자 함수에서 두 자리수 이상 숫자도 분리되도록 변경

--- src/main/java/calculator/NumericStringValidator.java
+++ src/main/java/calculator/NumericStringValidator.java
@@ -5,19 +5,16 @@ import java.util.List;
 
 public class NumericStringValidator {
     //표준 구분자와 숫자로만 이루어져있는지 확인
-    public static List<Number> digitsAndStandard(String values){
-        List<Number> digits = new ArrayList<>();
-        for(int i=0; i<values.length();i++){
-            char c = values.charAt(i);
-            if(i%2 == 0 && Character.isDigit(c)){
-                digits.add(Integer.parseInt(String.valueOf(c)));
-            }
-            else if (i % 2 == 1 && (values.charAt(i) == ':' || values.charAt(i) == ',')) {
-            } else {
-                throw new IllegalArgumentException();
-            }
+    public static List<Integer> digitsAndStandard(String values){
+        String[] digits;
+        //현재 두 자리 이상의 숫자의 경우에는 에러가 발생한다.
+        digits = values.split("[:,]");
+        List<Integer> intDigits = new ArrayList<>();
+        for(int i = 0; i< digits.length; i++){
+            int num = Integer.parseInt(digits[i]);
+            intDigits.add(num);
         }
-        return digits;
+        return intDigits;
     }
     public static String processValidator(String values){
         if(values.matches("^[0-9:,]+$")){
