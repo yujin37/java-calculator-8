00164ffaad5826ff20cf60edcc2ac3dbe2e17461
yujin37 <yj061628@gmail.com>
2025-10-20 00:38:50 +0900

    feat: 검증 후 리스트로 호출 할 경우 덧셈 계산 기능 추가

--- src/main/java/calculator/NumericStringPlus.java
+++ src/main/java/calculator/NumericStringPlus.java
@@ -1,4 +1,13 @@
 package calculator;
 
+import java.util.List;
+
 public class NumericStringPlus {
+    public static Integer digitPlusCalculator(List<Integer> integerBox){
+        Integer plusResult = 0;
+        for(int i = 0;i<integerBox.size();i++){
+            plusResult += integerBox.get(i);
+        }
+        return plusResult;
+    }
 }
