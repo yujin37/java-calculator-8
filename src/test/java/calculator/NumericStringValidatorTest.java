a6042c3de64bbca0fc56a5fe9299d2e18d005a14
yujin37 <yj061628@gmail.com>
2025-10-20 02:23:01 +0900

    refactor: 예외 케이스 입력 split 함수 분리 함수 인자 변경으로 인한 수정

--- src/test/java/calculator/NumericStringValidatorTest.java
+++ src/test/java/calculator/NumericStringValidatorTest.java
@@ -12,18 +12,18 @@ public class NumericStringValidatorTest extends NsTest {
     @Test
     void 커스텀_구분자_사용_알파벳() {
         String customAlpha = "//o\\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha, 'o')).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
     }
     @Test
     void 커스텀_구분자_사용_숫자() {
         String customAlpha = "//3\\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha, 'o')).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
     }
 
     @Test
     void 커스텀_구분자_사용_공백() {
         String customAlpha = "// \\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha, ' ')).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
     }
     @Test
     void 예외_테스트_커스텀_소수점() {
