eff698eb3f9e585bc8d3c2a2deabc6f19a3b25e2
yujin37 <yj061628@gmail.com>
2025-10-20 02:19:44 +0900

    refactor: 표준 및 스페셜 검증 함수 중복 부분 공통 함수로 따로 빼서 설정

--- src/main/java/calculator/NumericStringValidator.java
+++ src/main/java/calculator/NumericStringValidator.java
@@ -8,24 +8,24 @@ public class NumericStringValidator {
     //표준 구분자와 숫자로만 이루어져있는지 확인
     public static List<Integer> digitsAndStandard(String values){
         String[] digits;
-        //현재 두 자리 이상의 숫자의 경우에는 에러가 발생한다.
         digits = values.split("[:,]");
-        List<Integer> intDigits = new ArrayList<>();
-        for(int i = 0; i< digits.length; i++){
-            int num = Integer.parseInt(digits[i]);
-            intDigits.add(num);
-        }
-        return intDigits;
+
+        return parseDigits(digits);
     }
-    public static List<Integer> digitsAndSpecial(String values, char specialTag){
+    public static List<Integer> digitsAndSpecial(String values){
         String[] digits;
+        char specialTag = values.charAt(2);
         String tag = "[:," + Pattern.quote(String.valueOf(specialTag)) + "]";
         String cuttedValue = values.substring(values.indexOf("\\n") + 2);
         digits = cuttedValue.split(tag);
 
+        return parseDigits(digits);
+    }
+
+    public static List<Integer> parseDigits(String[] stringDigits){
         List<Integer> intDigits = new ArrayList<>();
-        for(int i = 0; i<digits.length;i++){
-            int num = Integer.parseInt(digits[i]);
+        for(int i = 0; i< stringDigits.length; i++){
+            int num = Integer.parseInt(stringDigits[i]);
             intDigits.add(num);
         }
         return intDigits;
@@ -33,7 +33,7 @@ public class NumericStringValidator {
     public static Integer processValidator(String values){
         List<Integer> splitSpecial;
         if (values.startsWith("//") && values.startsWith("\\n", 3)) {
-            splitSpecial = digitsAndSpecial(values, values.charAt(2));
+            splitSpecial = digitsAndSpecial(values);
         }
         else if (values.matches("^[0-9:,]+$")){
             splitSpecial = digitsAndStandard(values);
