cbc69a05d1f7bcaec62620d8202bdab921993441
yujin37 <yj061628@gmail.com>
2025-10-20 00:37:19 +0900

    feat: 특수 구분자 처리 기능 추가 및 종합 처리 함수 내 접근 수정

--- src/main/java/calculator/NumericStringValidator.java
+++ src/main/java/calculator/NumericStringValidator.java
@@ -2,6 +2,7 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class NumericStringValidator {
     //표준 구분자와 숫자로만 이루어져있는지 확인
@@ -16,15 +17,31 @@ public class NumericStringValidator {
         }
         return intDigits;
     }
-    public static String processValidator(String values){
-        if(values.matches("^[0-9:,]+$")){
-            System.out.println(digitsAndStandard(values));
-        }
-        else{
-            System.out.println("특수 구분자나 잘못된 값일 겁니다.");
-        }
+    public static List<Integer> digitsAndSpecial(String values, char specialTag){
+        String[] digits;
+        String tag = "[:," + Pattern.quote(String.valueOf(specialTag)) + "]";
+        String cuttedValue = values.substring(values.indexOf("\\n") + 2);
+        digits = cuttedValue.split(tag);
 
-        return "0";
+        List<Integer> intDigits = new ArrayList<>();
+        for(int i = 0; i<digits.length;i++){
+            int num = Integer.parseInt(digits[i]);
+            intDigits.add(num);
+        }
+        return intDigits;
+    }
+    public static Integer processValidator(String values){
+        List<Integer> splitSpecial;
+        if (values.startsWith("//") && values.startsWith("\\n", 3)) {
+            splitSpecial = digitsAndSpecial(values, values.charAt(2));
+        }
+        else if (values.matches("^[0-9:,]+$")){
+            splitSpecial = digitsAndStandard(values);
+        }else{
+            throw new IllegalArgumentException("입력이 잘못 되었습니다.");
+        }
+        return NumericStringPlus.digitPlusCalculator(splitSpecial);
+
     }
 
 }
