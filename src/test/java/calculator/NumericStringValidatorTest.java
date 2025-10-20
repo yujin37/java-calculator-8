6899107e15d1af3c0759acedb38cb1207a7fefaa
yujin37 <yj061628@gmail.com>
2025-10-20 10:16:44 +0900

    test: NumericStringValidator 클래스 접근 제한 변경으로 인한 테스트 코드 수정

--- src/test/java/calculator/NumericStringValidatorTest.java
+++ src/test/java/calculator/NumericStringValidatorTest.java
@@ -12,18 +12,18 @@ public class NumericStringValidatorTest extends NsTest {
     @Test
     void 커스텀_구분자_사용_알파벳() {
         String customAlpha = "//o\\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.processValidator(customAlpha)).isEqualTo(1);
     }
     @Test
     void 커스텀_구분자_사용_숫자() {
         String customAlpha = "//3\\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.processValidator(customAlpha)).isEqualTo(1);
     }
 
     @Test
     void 커스텀_구분자_사용_공백() {
         String customAlpha = "// \\n1";
-        assertThat(NumericStringValidator.digitsAndSpecial(customAlpha)).isEqualTo(Arrays.asList(1));
+        assertThat(NumericStringValidator.processValidator(customAlpha)).isEqualTo(1);
     }
     @Test
     void 예외_테스트_커스텀_소수점() {
@@ -46,6 +46,7 @@ public class NumericStringValidatorTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    // 아예 잘못된 입력 예제도 추가하기
     @Override
     public void runMain() {
         Application.main(new String[]{});
