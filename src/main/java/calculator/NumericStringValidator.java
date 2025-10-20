e25c6d2ebead581f7d2ca5e986491b686c2b35a4
yujin37 <yj061628@gmail.com>
2025-10-20 09:53:59 +0900

    style: processValidator 함수 내 else 구분 띄워쓰기 수정 및 입력 에러 메시지 수정

--- src/main/java/calculator/NumericStringValidator.java
+++ src/main/java/calculator/NumericStringValidator.java
@@ -34,11 +34,10 @@ public class NumericStringValidator {
         List<Integer> splitSpecial;
         if (values.startsWith("//") && values.startsWith("\\n", 3)) {
             splitSpecial = digitsAndSpecial(values);
-        }
-        else if (values.matches("^[0-9:,]+$")){
+        } else if (values.matches("^[0-9:,]+$")){
             splitSpecial = digitsAndStandard(values);
-        }else{
-            throw new IllegalArgumentException("입력이 잘못 되었습니다.");
+        } else {
+            throw new IllegalArgumentException("입력 형식이 잘못 되었습니다. 표준 구분자(:;)와 양수 숫자로 이루어지지 않았거나 특수 구분자 입력 규칙을 지키지 않았습니다."); // 알림 메시지 상세하게 변경하기
         }
         return NumericStringPlus.digitPlusCalculator(splitSpecial);
 
